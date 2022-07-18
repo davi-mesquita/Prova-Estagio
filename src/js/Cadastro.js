bac526998e075bde6f5a09ed9e0c159786f72c95
Davi Mesquita <davimesquita9@gmail.com>
2022-07-18 20:17:00 -0300

    Mexendo

--- src/js/Cadastro.js
+++ src/js/Cadastro.js
@@ -8,22 +8,36 @@ function Cadastro(){
 
     let lista = document.getElementById("Lista-de-Cadastro");
 
-    let Pessoas ={
+    let Pessoa ={
+        id : lista.childElementCount,
         nome: nome,
         email: email,
         nascimento: nascimento,
         telefone: telefone
+
     }
-    
+
+    let Pessoas = [];
+
+    Pessoas.push(Pessoa);
+
     if(nome !== "" && email !== "" && nascimento !== "" && telefone !== ""){
+        lista.innerText = '';
         for (let i = 0; i < Pessoas.length; i++) {
             let tr = lista.insertRow();
-            
+           
             let td_id = tr.insertCell();
             let td_nome = tr.insertCell();
             let td_email = tr.insertCell();
             let td_nascimento = tr.insertCell();
             let td_telefone = tr.insertCell();
+
+              
+            td_id.innerText =  this.Pessoas[i].id;
+            td_nome.innerText =  this.Pessoas[i].nome;
+            td_email.innerText =  this.Pessoas[i].email;
+            td_nascimento.innerText =  this.Pessoas[i].nascimento;
+            td_telefone.innerText =  this.Pessoas[i].telefone;
         }
     }else{
         alert("Preencha todos os campos!");
