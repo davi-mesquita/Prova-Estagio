828195bfafa63dc8369a34cd151637a2b8d4e0a0
Davi Mesquita <davimesquita9@gmail.com>
2022-07-18 03:03:50 -0300

    Arrumado

--- src/js/burguer.js
+++ src/js/burguer.js
@@ -1,8 +1,10 @@
 var btnMenu = document.getElementsByClassName("btn-menu");
 var Menu = document.getElementById("menu");
+var body = document.body;
 var barras = document.getElementById("teste");
 for (var i = 0; i < btnMenu.length; i++) {
     btnMenu[i].addEventListener('click', function () {
-        Menu.classList.toggle('menu-open');
+        body.classList.toggle('menu-open');
+        Menu.classList.toggle('menu-openned');
     });
 }
